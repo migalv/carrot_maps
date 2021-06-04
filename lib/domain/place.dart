915862982a4265beb447c9c50972fcaa42b7db62
migalv <migalv007@gmail.com>
2021-06-04 20:58:07 +0200

    Add comments to the Place data class

--- lib/domain/place.dart
+++ lib/domain/place.dart
@@ -3,10 +3,17 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'place.freezed.dart';
 
 @freezed
+
+/// Data class object that represents a place in a map
 abstract class Place with _$Place {
   const factory Place({
+    /// The given name to place
     required String name,
+
+    /// Coordinate for longitude
     required double longitude,
+
+    /// Coordinate for latitude
     required double latitude,
   }) = _Place;
 }
