18b09dc7406c484a92cb33820a72dbc969fd8b01
migalv <migalv007@gmail.com>
2021-06-05 13:08:48 +0200

    Add serialization to the Place Data Class

--- lib/domain/place.dart
+++ lib/domain/place.dart
@@ -1,12 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'place.freezed.dart';
+part 'place.g.dart';
 
 @freezed
 
 /// Data class object that represents a place in a map
-abstract class Place with _$Place {
+class Place with _$Place {
   const factory Place({
+    @JsonKey(ignore: true) String? id,
+
     /// The given name to place
     required String name,
 
@@ -16,4 +20,11 @@ abstract class Place with _$Place {
     /// Coordinate for latitude
     required double latitude,
   }) = _Place;
+
+  factory Place.fromJson(Map<String, dynamic> json) => _$PlaceFromJson(json);
+
+  factory Place.fromFirestore(DocumentSnapshot doc) {
+    return Place.fromJson(doc.data()! as Map<String, dynamic>)
+        .copyWith(id: doc.id);
+  }
 }
