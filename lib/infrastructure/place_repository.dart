dbe91c5a25ac67cbcde6f42c166d3ff7bf7ade80
migalv <migalv007@gmail.com>
2021-06-05 12:47:57 +0200

    Add Firestore as a dependency to Place Repository

--- lib/infrastructure/place_repository.dart
+++ lib/infrastructure/place_repository.dart
@@ -1,9 +1,15 @@
 import 'package:carrot_maps/domain/failures/place_repository_failure.dart';
 import 'package:carrot_maps/domain/i_place_repository.dart';
-import 'package:dartz/dartz.dart';
 import 'package:carrot_maps/domain/place.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
 
 class PlaceRepository implements IPlaceRepository {
+  final FirebaseFirestore _firestore;
+
+  /// Repository that handles Places from Cloud Firestore
+  PlaceRepository(this._firestore);
+
   @override
   Future<Either<PlaceRepositoryFailure, Unit>> create(Place place) {
     // TODO: implement create
