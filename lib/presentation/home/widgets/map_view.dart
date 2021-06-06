934d3074559f6d3bfaf56cee45dc5960b7f9def4
migalv <migalv007@gmail.com>
2021-06-06 20:55:25 +0200

    Add code to retreive user's current location
    The map will also animate and zoom to the user's location

--- lib/presentation/home/widgets/map_view.dart
+++ lib/presentation/home/widgets/map_view.dart
@@ -1,15 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
-class MapView extends StatelessWidget {
+class MapView extends StatefulWidget {
   const MapView({Key? key}) : super(key: key);
 
   @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  final Location location = Location();
+
+  @override
   Widget build(BuildContext context) {
-    return const GoogleMap(
-      initialCameraPosition: CameraPosition(
-        target: LatLng(45.521563, -122.677433),
-      ),
+    return GoogleMap(
+      initialCameraPosition: const CameraPosition(target: LatLng(0, 0)),
+      onMapCreated: (controller) async {
+        bool _serviceEnabled;
+        PermissionStatus _permissionGranted;
+        LocationData _locationData;
+
+        _serviceEnabled = await location.serviceEnabled();
+        if (!_serviceEnabled) {
+          _serviceEnabled = await location.requestService();
+          if (!_serviceEnabled) {
+            return;
+          }
+        }
+
+        _permissionGranted = await location.hasPermission();
+        if (_permissionGranted == PermissionStatus.denied) {
+          _permissionGranted = await location.requestPermission();
+          if (_permissionGranted != PermissionStatus.granted) {
+            return;
+          }
+        }
+
+        _locationData = await location.getLocation();
+
+        controller.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: LatLng(
+                _locationData.latitude ?? 0,
+                _locationData.longitude ?? 0,
+              ),
+              zoom: 10,
+            ),
+          ),
+        );
+      },
     );
   }
 }
