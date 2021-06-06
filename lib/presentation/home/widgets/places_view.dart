37139c0111f8bb8c1b8a93dcd33a18fa923b8975
migalv <migalv007@gmail.com>
2021-06-06 16:45:53 +0200

    Create the Places View Form

--- lib/presentation/home/widgets/places_view.dart
+++ lib/presentation/home/widgets/places_view.dart
@@ -5,6 +5,57 @@ class PlacesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Form(
+      child: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Text(
+              "Introduce los detalles del nuevo lugar",
+              style: Theme.of(context).textTheme.headline6,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                labelText: "Nombre del lugar",
+                border: OutlineInputBorder(),
+              ),
+              textInputAction: TextInputAction.next,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Longitud",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: "Latitud",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.done,
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Crear lugar"),
+          ),
+        ],
+      ),
+    );
   }
 }
