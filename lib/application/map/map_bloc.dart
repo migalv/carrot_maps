3a16ddcff440e7ebd146e6d62f850fcb564488a5
migalv <migalv007@gmail.com>
2021-06-07 17:44:47 +0200

    Implement Map Bloc to load places on the map

---
+++ lib/application/map/map_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:carrot_maps/domain/i_place_repository.dart';
+import 'package:carrot_maps/domain/place.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+part 'map_event.dart';
+part 'map_state.dart';
+part 'map_bloc.freezed.dart';
+
+@injectable
+class MapBloc extends Bloc<MapEvent, MapState> {
+  final IPlaceRepository _placeRepository;
+
+  MapBloc(this._placeRepository) : super(const _Initial());
+
+  @override
+  Stream<MapState> mapEventToState(
+    MapEvent event,
+  ) async* {
+    yield* event.when(loadStarted: () async* {
+      yield* _placeRepository.placesStream.map(
+        (failureOrPlaces) => failureOrPlaces.fold(
+          (failure) => MapState.loadFailure(
+            failure.when(
+              unexpected: () => "Ocurrió un error inesperado",
+              insufficientPermissions: () =>
+                  "Usuario sin los permisos necesarios",
+            ),
+          ),
+          (places) => MapState.loadSuccess(places),
+        ),
+      );
+    });
+  }
+}
--- lib/presentation/home/widgets/map_view.dart
+++ lib/presentation/home/widgets/map_view.dart
@@ -1,25 +1,51 @@
+import 'package:carrot_maps/application/map/map_bloc.dart';
+import 'package:carrot_maps/injection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-class MapView extends StatefulWidget {
+class MapView extends StatelessWidget {
   const MapView({Key? key}) : super(key: key);
 
   @override
-  _MapViewState createState() => _MapViewState();
-}
-
-class _MapViewState extends State<MapView> {
-  final Location location = Location();
-
-  @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<MapBloc>()..add(const MapEvent.loadStarted()),
+      child: BlocConsumer<MapBloc, MapState>(
+        listener: _blocListener,
+        builder: (context, state) => _buildMap(state),
+      ),
+    );
+  }
+
+  Widget _buildMap(MapState state) {
+    Set<Marker> markers = {};
+
+    state.when(
+      initial: () {},
+      loadSuccess: (places) {
+        markers = places
+            .map(
+              (place) => Marker(
+                markerId: MarkerId(place.name),
+                infoWindow: InfoWindow(title: place.name),
+                position: LatLng(place.latitude, place.longitude),
+              ),
+            )
+            .toSet();
+      },
+      loadFailure: (failureMessage) {},
+    );
+
     return GoogleMap(
       initialCameraPosition: const CameraPosition(target: LatLng(0, 0)),
+      markers: markers,
       onMapCreated: (controller) async {
         bool _serviceEnabled;
         PermissionStatus _permissionGranted;
         LocationData _locationData;
+        final Location location = Location();
 
         _serviceEnabled = await location.serviceEnabled();
         if (!_serviceEnabled) {
@@ -53,4 +79,15 @@ class _MapViewState extends State<MapView> {
       },
     );
   }
+
+  void _blocListener(BuildContext context, MapState state) => state.when(
+        initial: () {},
+        loadSuccess: (places) {},
+        loadFailure: (failureMessage) =>
+            ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(failureMessage),
+          ),
+        ),
+      );
 }
