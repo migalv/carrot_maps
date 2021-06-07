4080e1e39a8ffa1ac066ba76b70ca2292dbe53f1
migalv <migalv007@gmail.com>
2021-06-07 22:15:09 +0200

    Add DotEnv library
    This library is perfect to load environment variables.
    I will use it to load the open weather api key

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as dotenv;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await Firebase.initializeApp();
+  await dotenv.load();
   configureInjection(Environment.dev);
 
   runApp(
