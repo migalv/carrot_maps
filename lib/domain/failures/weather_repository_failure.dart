de87ca3686b780b56ff7773c400d811f8c196e24
migalv <migalv007@gmail.com>
2021-06-08 13:09:04 +0200

    Add error handling to the weather repository

--- lib/domain/failures/weather_repository_failure.dart
+++ lib/domain/failures/weather_repository_failure.dart
@@ -1,9 +1,19 @@
+import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'weather_repository_failure.freezed.dart';
 
 @freezed
 class WeatherRepositoryFailure with _$WeatherRepositoryFailure {
-  const factory WeatherRepositoryFailure.serverError({required int errorCode}) =
-      _ServerError;
+  const factory WeatherRepositoryFailure.serverError({
+    int? errorCode,
+  }) = _ServerError;
+
+  const factory WeatherRepositoryFailure.noResponse({
+    String? errorMessage,
+  }) = _NoResponse;
+
+  const factory WeatherRepositoryFailure.unknownServerResponse({
+    int? errorCode,
+  }) = _UnknownServerResponse;
 }
--- lib/infrastructure/weather_repository.dart
+++ lib/infrastructure/weather_repository.dart
@@ -14,23 +14,46 @@ class WeatherRepository implements IWeatherRepository {
   @override
   Future<Either<WeatherRepositoryFailure, double>> getTemperatureForCoordinates(
       {required double latitude, required double longitude}) async {
-    final response = await _dio.get(
-      "/data/2.5/weather",
-      queryParameters: {
-        'lat': latitude,
-        'lon': longitude,
-        'appid': dotenv.env["OPEN_WEATHER_API_KEY"],
-        'units': 'metric',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      return Right(response.data["main"]["temp"] as double);
+    late Response<Map<String, dynamic>> response;
+    try {
+      response = await _dio.get<Map<String, dynamic>>(
+        "/data/2.5/weather",
+        queryParameters: {
+          'lat': latitude,
+          'lon': longitude,
+          'appid': dotenv.env["OPEN_WEATHER_API_KEY"],
+          'units': 'metric',
+        },
+      );
+      if (response.statusCode == 200) {
+        if (response.data != null) {
+          return Right(response.data!["main"]["temp"] as double);
+        }
+      }
+    } on DioError catch (e) {
+      late WeatherRepositoryFailure failure;
+      if (e.response != null) {
+        failure = WeatherRepositoryFailure.serverError(
+          errorCode: e.response!.statusCode,
+        );
+      } else {
+        failure = WeatherRepositoryFailure.noResponse(
+          errorMessage: e.message,
+        );
+      }
+      return Left(failure);
     }
 
-    final failure = WeatherRepositoryFailure.serverError(
-      errorCode: response.statusCode ?? 404,
+    final failure = WeatherRepositoryFailure.unknownServerResponse(
+      errorCode: response.statusCode,
     );
     return Left(failure);
   }
 }
+
+const String serverErrorMessage =
+    "Server responded with error.{e.response!.statusCode != null ? ' ({e.response!.statusCode})' : ''}";
+const String noResponseErrorMessage =
+    "Server did not respond.{e.message != " " ? ' Error: {e.message}' : ''}";
+const String unknownServerResponseErrorMessage =
+    "Received an unknown server response {e.response!.statusCode != null ? ' ({e.response!.statusCode})' : ''}";
